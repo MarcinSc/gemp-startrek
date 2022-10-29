476c065db9ec8a9fc39da117e9c3536235734cc6
MarcinSc <marcins78@gmail.com>
2022-10-30 06:16:36 +0700

    Better positioning of the nameplates

--- core/src/main/java/com/gempukku/startrek/game/PlayerInfoRenderingSystem.java
+++ core/src/main/java/com/gempukku/startrek/game/PlayerInfoRenderingSystem.java
@@ -4,7 +4,9 @@ import com.artemis.Aspect;
 import com.artemis.BaseEntitySystem;
 import com.artemis.Entity;
 import com.artemis.utils.IntBag;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.gempukku.libgdx.lib.artemis.camera.CameraSystem;
 import com.gempukku.libgdx.lib.artemis.spawn.SpawnSystem;
@@ -55,6 +57,8 @@ public class PlayerInfoRenderingSystem extends BaseEntitySystem {
     }
 
     private void createPlayerNameplates() {
+        Camera camera = cameraSystem.getCamera();
+
         IntBag playerIds = getSubscription().getEntities();
         for (int i = 0; i < playerIds.size(); i++) {
             Entity playerEntity = world.getEntity(playerIds.get(i));
@@ -77,11 +81,18 @@ public class PlayerInfoRenderingSystem extends BaseEntitySystem {
 
             PlayerPosition playerPosition = playerPositionSystem.getPlayerPosition(player.getName());
 
-            float zTranslate = playerPosition == PlayerPosition.Lower ? 0.7f : -0.7f;
+            float zTranslate = playerPosition == PlayerPosition.Lower ? 0.13f : -0.13f;
+
+            Vector3 cameraRight = new Vector3(camera.direction).crs(camera.up);
 
             transformSystem.setTransform(nameplateEntity,
                     new Matrix4()
-                            .translate(-6f, 0f, zTranslate));
+                            .translate(new Vector3(camera.position))
+                            .translate(new Vector3(camera.direction).scl(3f))
+                            .translate(new Vector3(cameraRight).scl(-1.2f))
+                            .translate(new Vector3(camera.up).scl(-zTranslate))
+                            .scl(0.2f)
+                            .rotate(1, 0, 0, 10f));
 
             addedPlayers++;
         }
