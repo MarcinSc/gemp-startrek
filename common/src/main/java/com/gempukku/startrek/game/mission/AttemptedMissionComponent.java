ee125da6d51eee3293b9afb42cf8940f0dfbee23
MarcinSc <marcins78@gmail.com>
2022-11-27 13:51:00 +0700

    Working on mission attempts

--- common/src/main/java/com/gempukku/startrek/game/mission/AttemptedMissionComponent.java
+++ common/src/main/java/com/gempukku/startrek/game/mission/AttemptedMissionComponent.java
@@ -2,12 +2,23 @@ package com.gempukku.startrek.game.mission;
 
 import com.artemis.Component;
 import com.badlogic.gdx.utils.Array;
-import com.gempukku.libgdx.network.server.config.annotation.ReplicateToClients;
+import com.gempukku.libgdx.network.server.config.annotation.OwnedComponent;
+import com.gempukku.libgdx.network.server.config.annotation.ReplicateToOwner;
 
-@ReplicateToClients
-public class AttemptedMissionComponent extends Component {
+@ReplicateToOwner
+public class AttemptedMissionComponent extends Component implements OwnedComponent {
+    private String owner;
     private String missionId;
     private Array<String> attemptingPersonnel = new Array<>();
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean isOwnedBy(String username) {
+        return username.equals(owner);
+    }
 
     public String getMissionId() {
         return missionId;
--- server/src/main/java/com/gempukku/startrek/server/game/effect/mission/AssignAttemptingPersonnelEffect.java
+++ server/src/main/java/com/gempukku/startrek/server/game/effect/mission/AssignAttemptingPersonnelEffect.java
@@ -11,6 +11,7 @@ import com.gempukku.startrek.game.Memory;
 import com.gempukku.startrek.game.ValidateUtil;
 import com.gempukku.startrek.game.filter.CardFilteringSystem;
 import com.gempukku.startrek.game.mission.AttemptedMissionComponent;
+import com.gempukku.startrek.game.player.PlayerResolverSystem;
 import com.gempukku.startrek.server.game.effect.GameEffectComponent;
 import com.gempukku.startrek.server.game.effect.OneTimeEffectSystem;
 import com.gempukku.startrek.server.game.stack.ExecutionStackSystem;
@@ -19,6 +20,7 @@ import java.util.function.Consumer;
 
 public class AssignAttemptingPersonnelEffect extends OneTimeEffectSystem {
     private CardFilteringSystem cardFilteringSystem;
+    private PlayerResolverSystem playerResolverSystem;
     private IdProviderSystem idProviderSystem;
     private ExecutionStackSystem executionStackSystem;
     private EventSystem eventSystem;
@@ -29,6 +31,7 @@ public class AssignAttemptingPersonnelEffect extends OneTimeEffectSystem {
 
     @Override
     protected void processOneTimeEffect(Entity sourceEntity, GameEffectComponent gameEffect, Memory memory) {
+        String player = playerResolverSystem.resolvePlayerUsername(sourceEntity, memory, gameEffect.getDataString("player"));
         String memoryName = gameEffect.getDataString("memory");
         String filter = gameEffect.getDataString("filter");
         Array<String> personnelIds = new Array<>();
@@ -42,6 +45,7 @@ public class AssignAttemptingPersonnelEffect extends OneTimeEffectSystem {
 
         Entity attemptedMissionEntity = executionStackSystem.getTopMostStackEntityWithComponent(AttemptedMissionComponent.class);
         AttemptedMissionComponent attemptedMission = attemptedMissionEntity.getComponent(AttemptedMissionComponent.class);
+        attemptedMission.setOwner(player);
         attemptedMission.getAttemptingPersonnel().addAll(personnelIds);
         eventSystem.fireEvent(EntityUpdated.instance, attemptedMissionEntity);
     }
@@ -49,8 +53,9 @@ public class AssignAttemptingPersonnelEffect extends OneTimeEffectSystem {
     @Override
     public void validate(JsonValue effect) {
         ValidateUtil.effectExpectedFields(effect,
-                new String[]{"filter", "memory"},
+                new String[]{"filter", "memory", "player"},
                 new String[]{});
         cardFilteringSystem.validateFilter(effect.getString("filter"));
+        playerResolverSystem.validatePlayer(effect.getString("player"));
     }
 }
